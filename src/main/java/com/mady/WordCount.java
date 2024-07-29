e72079bea3b8feb4c81f539a0483da9ee9f3b951
unknown <chittemmadhusudhanareddy@gmail.com>
2024-07-29 12:35:56 -0600

    spark wordcount java program

--- src/main/java/com/mady/WordCount.java
+++ src/main/java/com/mady/WordCount.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 
 import scala.Tuple2;
 
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
@@ -14,17 +13,21 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+
+/**
+ * @author Madhu
+ */
 public class WordCount {
     public static void main(String[] args) throws Exception {
         String inputFile = args[0];
         String outputFile = args[1];
         // Create a Java Spark Context.
         SparkConf conf = new SparkConf().setAppName("wordCount");
-        conf.setMaster("local[*]");
+        conf.setMaster("local[1]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         // Load our input data.
-        JavaRDD<String> input = sc.textFile(inputFile);
+        JavaRDD<String> input = sc.textFile(inputFile, 2);
         // Split up into words.
         JavaRDD<String> words = input.flatMap(new FlatMapFunction<String, String>() {
             @Override
@@ -33,13 +36,20 @@ public class WordCount {
             }
         });
         // Transform into word and count.
-        JavaPairRDD<String, Integer> counts = words.mapToPair(
-                new PairFunction<String, String, Integer>(){
-                    public Tuple2<String, Integer> call(String x){
+        JavaPairRDD<String, Integer> pairRdds = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    public Tuple2<String, Integer> call(String x) {
                         return new Tuple2(x, 1);
-                    }}).reduceByKey(new Function2<Integer, Integer, Integer>(){
-            public Integer call(Integer x, Integer y){ return x + y;}});
+                    }
+                });
+
+        JavaPairRDD<String, Integer> counts = pairRdds.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer x, Integer y) {
+                return x + y;
+            }
+        }, 1);
         // Save the word count back out to a text file, causing evaluation.
         counts.saveAsTextFile(outputFile);
+
     }
 }
